b53af220a26c9ef0d6d2b4659bbb01f1a606e867
Niya Panamdanam <niya@findniya.com>
2019-01-09 23:13:14 -0500

    debug removed

--- server/routes/api/GooglePlaces.js
+++ server/routes/api/GooglePlaces.js
@@ -5,7 +5,6 @@ module.exports = (app) => {
   app.post('/find-food', (req, res) => {
     const { lat, lon } = req.body
     const mapKey = process.env.MAP_KEY
-    console.log(mapKey)
     const apiUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lon}&radius=1500&type=restaurant&opennow=true&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=${mapKey}`
     axios.get(apiUrl)
       .then(data => {
