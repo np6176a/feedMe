34c3ef80c235620ca326c21ca44c52dae7106b0c
Niya Panamdanam <niya@findniya.com>
2019-01-09 23:12:49 -0500

    route clean up

--- server/routes/api/GooglePlaces.js
+++ server/routes/api/GooglePlaces.js
@@ -4,8 +4,9 @@ require('dotenv').config()
 module.exports = (app) => {
   app.post('/find-food', (req, res) => {
     const { lat, lon } = req.body
-    const MAP_KEY = process.env.MAP_KEY
-    const apiUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lon}&radius=1500&type=restaurant&opennow=true&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=${MAP_KEY}`
+    const mapKey = process.env.MAP_KEY
+    console.log(mapKey)
+    const apiUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lon}&radius=1500&type=restaurant&opennow=true&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=${mapKey}`
     axios.get(apiUrl)
       .then(data => {
         res.send(data.data)
